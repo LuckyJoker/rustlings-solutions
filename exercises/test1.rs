f04e77893a89e030f9c007b082726ca749447394
LuckyJoker <1046944896@qq.com>
2020-03-03 23:23:06 +0800

    all solutions

---
+++ exercises/test1.rs
@@ -0,0 +1,30 @@
+// test1.rs
+// This is a test for the following sections:
+// - Variables
+// - Functions
+
+// Mary is buying apples. One apple usually costs 2 dollars, but if you buy
+// more than 40 at once, each apple only costs 1! Write a function that calculates
+// the price of an order of apples given the order amount. No hints this time!
+
+
+// Put your function here!
+// fn ..... {
+
+// Don't modify this function!
+#[test]
+fn verify_test() {
+    let price1 = calculate_apple_price(35);
+    let price2 = calculate_apple_price(65);
+
+    assert_eq!(70, price1);
+    assert_eq!(65, price2);
+}
+
+fn calculate_apple_price(counts: i32) -> i32 {
+    if counts > 40 {
+        counts
+    } else {
+        counts * 2
+    }
+}
